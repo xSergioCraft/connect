b88c7230fd02e91ae96ba88f84d7d0a8fbcb5915
xSergioCraft <sergioalixruiz@gmail.com>
2026-09-21 20:51:52 +0200

    Update connect.py

--- connect/connect.py
+++ connect/connect.py
@@ -1,6 +1,4 @@
-import discord
 from discord.ext import commands
-
 from core import checks
 
 
@@ -13,47 +11,7 @@ class Connect(commands.Cog):
     @commands.command(name="connect")
     @checks.thread_only()
     async def connect(self, ctx):
-        """Connect to the current Modmail thread."""
-
-        embed = discord.Embed(
-            title="🧡 MIGROX SUPPORT",
-            description=(
-                "**Your support request has been connected.**\n\n"
-                "A member of the **Migros Corporation Support Team** "
-                "is now connected to your request and will assist you shortly."
-            ),
-            colour=discord.Colour.orange()
-        )
-
-        embed.add_field(
-            name="👤 Support Representative",
-            value=ctx.author.mention,
-            inline=False
-        )
-
-        embed.add_field(
-            name="🕒 Connected",
-            value=discord.utils.utcnow().strftime(
-                "%d %B %Y • %H:%M UTC"
-            ),
-            inline=False
-        )
-
-        embed.add_field(
-            name="📌 Please Note",
-            value=(
-                "Please remain in this channel while your request "
-                "is being handled by our Support Team."
-            ),
-            inline=False
-        )
-
-        embed.set_footer(
-            text="Migros Corporation • Migrox Support"
-        )
-
-        # Send through Modmail's thread reply system.
-        await ctx.thread.reply(embed=embed)
+        await ctx.send("🧡 **MIGROX SUPPORT**\n\nYour support request has been connected.")
 
 
 def setup(bot):
