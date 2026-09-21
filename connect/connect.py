721faa90314aa66ca1f1291a0dfd890d7070ab8c
xSergioCraft <sergioalixruiz@gmail.com>
2026-09-21 20:54:40 +0200

    Improve connect command with detailed embed response
    Enhanced the connect command to provide detailed support information and an embed message.

--- connect/connect.py
+++ connect/connect.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from core import checks
 
@@ -11,8 +12,45 @@ class Connect(commands.Cog):
     @commands.command(name="connect")
     @checks.thread_only()
     async def connect(self, ctx):
-        await ctx.send("🧡 **MIGROX SUPPORT**\n\nYour support request has been connected.")
+        embed = discord.Embed(
+            title="🧡 MIGROX SUPPORT",
+            description=(
+                "**Your support request has been connected.**\n\n"
+                "A member of the **Migros Corporation Support Team** "
+                "is now connected to your request and will assist you shortly."
+            ),
+            colour=discord.Colour.orange()
+        )
+
+        embed.add_field(
+            name="👤 Support Representative",
+            value=ctx.author.mention,
+            inline=False
+        )
+
+        embed.add_field(
+            name="🕒 Connected",
+            value=discord.utils.utcnow().strftime(
+                "%d %B %Y • %H:%M UTC"
+            ),
+            inline=False
+        )
+
+        embed.add_field(
+            name="📌 Please Note",
+            value=(
+                "Please remain in this channel while your request "
+                "is being handled by our Support Team."
+            ),
+            inline=False
+        )
+
+        embed.set_footer(
+            text="Migros Corporation • Migrox Support"
+        )
+
+        await ctx.thread.reply(embed=embed)
 
 
-def setup(bot):
-    bot.add_cog(Connect(bot))
+async def setup(bot):
+    await bot.add_cog(Connect(bot))
